e80a4bc7a38153b9b3d18b44e69cc58d07261330
Matthew Cross <mattyc246@gmail.com>
2019-06-20 11:30:40 +0800

    implemented fonts

---
+++ src/components/NavBar.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  handleDropdown = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="transparent" className="border-bottom" light expand="md">
+          <NavbarBrand className="alt-font">Instagram</NavbarBrand>
+          <NavbarToggler onClick={() => this.handleDropdown()} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">News Feed</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Components</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  <i class="far fa-user-circle" />
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>View Profile</DropdownItem>
+                  <DropdownItem>View Settings</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Logout</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
