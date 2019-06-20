a3eddc6fb9fb5f33737e4acbdd79295515d27276
Matthew Cross <mattyc246@gmail.com>
2019-06-20 17:09:12 +0800

    remove hard coded user out

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentUser: true,
+      currentUser: null,
       signUpFormData: {
         username: "",
         email: "",
