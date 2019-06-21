08a92db99aef991f643b673d1caed8fe4b3daa90
Matthew Cross <mattyc246@gmail.com>
2019-06-21 11:43:16 +0800

    moved logic from app to homepage

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BroswerRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BroswerRouter>
+  <BrowserRouter>
     <App />
-  </BroswerRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
